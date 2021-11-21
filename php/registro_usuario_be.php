c0408cb54c7513d9fec47baa00abe64e35b5cd01
GiovanniAS <blasgivx@gmail.com>
2021-11-21 14:26:43 -0600

    verificacion de correo y mensaje de alerta
    se hace la verificación del correo y se manda la alerta

--- php/registro_usuario_be.php
+++ php/registro_usuario_be.php
@@ -2,13 +2,24 @@
     include 'connection.php';
 
     $nombre_completo = $_POST['nombre_completo'];
-    $corrreo = $_POST['correo'];
+    $correo = $_POST['correo'];
     $usuario = $_POST['usuario'];
     $password = $_POST['password'];
 
     $query = "INSERT INTO usuarios(nombre_completo, correo, usuario, password)
-              VALUES('$nombre_completo','$corrreo','$usuario','$password')";
+              VALUES('$nombre_completo','$correo','$usuario','$password')";
     
+    //Verificar que el correo no se repita en la db
+    $verificar_correo = mysqli_query($conn, "SELECT * FROM usuarios WHERE correo = '$correo' ");
+    if(mysqli_num_rows($verificar_correo) > 0){
+       echo '
+            <script>
+                alert("Este correo ya esta registrado, intenta con otro diferente");
+                window.location = "../index.php";
+            </script>
+       '; 
+    }
+
     $ejecutar = mysqli_query($conn, $query);
 
     if($ejecutar){
