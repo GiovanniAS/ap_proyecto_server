1ddca9cab075ab5b8df2af295588ca21b83dda81
GiovanniAS <blasgivx@gmail.com>
2021-11-25 21:29:15 -0600

    header bienvenida
    se puso un encabezado en la pagina

--- bienvenida.php
+++ bienvenida.php
@@ -14,26 +14,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bienvenido usuario</title>
+    <link rel="stylesheet" href="assets/css/styles2.css">
 </head>
 <body>
     <header>
         <div class="header-content">
             <div class="logo">
-                <h1>Bienvenido</h1>
+                <h1>Bienvenido<b>User</b></h1>
             </div>
 
             <div class="menu">
                 <nav>
                     <ul>
                         <li><a href="#">Inicio</a></li>
-                        <li><a href="#">ETC</a></li>
+                        <li><a href="#">Etc</a></li>
                         <li><a href="#">About</a></li>
-                        <li><a href="#">Contacto</a></li>
+                        <li class="menu-selected"><a href="#" class="text-menu-selected">Contacto</a></li>
+                        <li><a href="php/cerrar_sesion.php">Cerrar sesion</a></li>
                     </ul>
                 </nav>
             </div>
         </div>
     </header>
-    <a href="php/cerrar_sesion.php">Cerrar sesion</a>
+    <!--<a href="php/cerrar_sesion.php">Cerrar sesion</a>-->
 </body>
 </html>
